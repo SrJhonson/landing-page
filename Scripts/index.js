92d6765174075f8392c222afab3175b373c999ab
Jhonatan Dias Farias <diasjhonatan24@gmail.com>
2023-08-31 21:08:51 -0300

    ajuste do botão dark

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,8 @@
 const navbar = document.querySelector('.navbar');
 const mobileNavBar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
+const dark = document.querySelector('checkbox');
+const chk = document.getElementById('chk');
 
 button.addEventListener('click', function () {
     mobileNavBar.classList.toggle('active');
@@ -10,12 +12,10 @@ button.addEventListener('click', function () {
 window.addEventListener('scroll', function () {
     if (this.window.pageYOffset > 0) return navbar.classList.add('active');
     return navbar.classList.remove('active');
-})
-
-const chk = document.getElementById('chk')
+});
 
 chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-})
+  document.body.classList.toggle('dark');
+});
 
 const scrollanima = document.querySelectorAll('[data-anima="scroll"]');
